f988cfe50059e0d2f1a46722d8850f2a35e0dfd9
Ukesh Rajasekar <z5288900@ad.unsw.edu.au>
2021-09-07 00:07:22 +1000

    Added auth routing

---
+++ src/pages/public/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import loading from '../../assets/loading.gif';
+// import logo from '../../assets/logo.png';
+import logotext from '../../assets/logotext.png';
+import LoginButton from '../../components/LoginButton';
+import LogoutButton from '../../components/LogoutButton';
+import Profile from '../../components/Profile';
+
+export default function Home() {
+   return (
+      <div>
+         <header className='App-header'>
+            <img src={loading} className='App-logo' alt='logo' />
+            <img src={logotext} className='App-logo-text' alt='logo' />
+
+            <LoginButton />
+            <LogoutButton />
+            <Profile />
+         </header>
+      </div>
+   );
+}
